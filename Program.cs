96914a0c218fa70fbb97f0ea2b6257200f79f3db
unknown <shake.hakobyan23@gmail.com>
2026-03-07 12:07:37 +0400

    moved AppDataContext to Data folder

--- Program.cs
+++ Program.cs
@@ -1,22 +1,18 @@
-using CombinationGeneratorAPI.Models.Entities;
+using CombinationGeneratorAPI.Data;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Register DbContext with PostgreSQL
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-// Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
